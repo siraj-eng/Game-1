f421e5cd36ac2fd7a38403be69c78d948741c56a
siraj-eng <wahhajsiraj16@gmail.com>
2024-11-11 18:22:22 +0300

    save current work

--- main.js
+++ main.js
@@ -4,16 +4,19 @@ dropdown.addEventListener('change', function(e){
     playerState = e.target.value;
 })
 
+//Creation of the canvas
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
 
+//Player Image
 const playerImage = new Image();
 playerImage.src = "shadow_dog.png";
 const spriteWidth = 575;
 const spriteHeight = 523;
 
+//creation of the game frame states
 let gameFrame = 0;
 const staggerFrame = 5;
 const spriteAnimations = [];
@@ -73,6 +76,7 @@ animationStates.forEach((state, index) => {
 });
 console.log(spriteAnimations);
 
+//function states
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     let position = Math.floor(gameFrame/staggerFrame) % spriteAnimations[playerState].loc.length;
